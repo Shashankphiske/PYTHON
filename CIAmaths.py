6d184dddc81680a2b1b2ee319482fe81fc95416f
Shashank Phiske <shashankphiske@gmail.com>
2022-12-07 22:40:24 +0530

    Modified file

---
+++ CIAmaths.py
@@ -0,0 +1,11 @@
+# This is a sample project only refer to this if you want to understand how to the code in future will work
+
+# install numpy , scipy , sympy , matplotlib
+# for more info about these libraries refer to google and understand it
+import scipy as sp
+import numpy as np
+import sympy as sym
+import matplotlib.pyplot as plot
+from scipy.misc import derivative
+
+x,a,b,c = smp.symbols('a,b,c,x' , real=True) # Here we defined our variables we are going to use 
